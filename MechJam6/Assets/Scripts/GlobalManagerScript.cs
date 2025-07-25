95bf374f9f3a8d72c964a23bbbee8223c569c868
Daniel <daniel.a.polen@gmail.com>
2025-07-24 20:08:16 -0700

    game maanager setup

---
+++ MechJam6/Assets/Scripts/GlobalManagerScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GlobalManagerScript : MonoBehaviour
+{
+    public int playerHealth = 4;
+    public int mechHealth = 4;
+    public int waterAmount = 0;
+    public int maxWaterAmount = 100;
+
+    public int playerINT = 0;
+    public int playerSTR = 0;
+    public int playerCHA = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
