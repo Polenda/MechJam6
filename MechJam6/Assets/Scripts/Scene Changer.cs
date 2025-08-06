7e7ee16a3fa81e3b288d53996bcea73426d3a3b8
Daniel <daniel.a.polen@gmail.com>
2025-08-05 20:40:12 -0700

    gameplay addition and fixing

--- MechJam6/Assets/Scripts/Scene Changer.cs
+++ MechJam6/Assets/Scripts/Scene Changer.cs
@@ -12,6 +12,9 @@ public class csUi : MonoBehaviour
 
     [SerializeField] private TownManagerScript townManagerScript;
     [SerializeField] private Sprite map;
+    [SerializeField] private Sprite homeMap;
+    [SerializeField] private SpriteRenderer mapRenderer;
+    [SerializeField] private GlobalManagerScript globalManager;
 
     public void SceneChanger()
     {
@@ -28,9 +31,33 @@ public class csUi : MonoBehaviour
     {
         Debug.Log("Travel button clicked, starting fade to black");
         yield return StartCoroutine(fadeScript.FadeToBlack());
+        townManagerScript.townObjectParent.SetActive(true);
         townManagerScript.townDialogueScript.NPCSpriteRenderer.enabled = false;
         townManagerScript.townDialogueScript.NPCDialoguePanel.SetActive(false);
         townManagerScript.townObjectParent.SetActive(true);
+        mapRenderer.sprite = map;
+        globalManager.Refresh();
         StartCoroutine(fadeScript.FadeToClear());
+
+    }
+
+    public void homeButton()
+    {
+        StartCoroutine(thisForFade());
+    }
+    IEnumerator thisForFade()
+    {
+        Debug.Log("Home button clicked, starting fade to black");
+        yield return StartCoroutine(fadeScript.FadeToBlack());
+        townManagerScript.townDialogueScript.NPCSpriteRenderer.enabled = false;
+        townManagerScript.townDialogueScript.NPCDialoguePanel.SetActive(false);
+        townManagerScript.townObjectParent.SetActive(false);
+        mapRenderer.sprite = homeMap;
+        globalManager.maxWaterAmount -= 20;
+        globalManager.waterAmount = globalManager.maxWaterAmount;
+        
+        globalManager.Refresh();
+        StartCoroutine(fadeScript.FadeToClear());
+        
     }
 }
